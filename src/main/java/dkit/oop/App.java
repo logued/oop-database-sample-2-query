ae1268257de0336e942cdee11a3a40752041cd13
Dermot Logue <dermot.logue@dkit.ie>
2025-01-22 10:15:22 +0000

    Updated dependencies in pom.xml, added some comments to code.

--- src/main/java/dkit/oop/App.java
+++ src/main/java/dkit/oop/App.java
@@ -1,26 +1,24 @@
 package dkit.oop;
-/**
- * Connect to Database and execute embedded SELECT Query.
- * Required: 
- * You MUST Start the MySql Server (from XAMPP) and create and populate the table "Customers", 
- *  with fields: custID, firstName, lastName, dob
- * The SQL code is given below (and is available in text file format on Moodle)
- *
- *
- DROP TABLE IF EXISTS customers;
 
-CREATE TABLE customers(custID int NOT NULL AUTO_INCREMENT,
-             firstName VARCHAR(20),
-             lastName VARCHAR(50),
-             dob date,
-	PRIMARY KEY (custID));
+//  Connect to Database and execute embedded SELECT Query.
+//  Required:
+//  You MUST Start the MySql Server (from XAMPP) and create and populate
+//  the table "Customers",  with fields: custID, firstName, lastName, dob
+//  The SQL code is given below (and is available in text file format on Moodle)
+//
+// DROP TABLE IF EXISTS customers;
+//
+// CREATE TABLE customers(custID int NOT NULL AUTO_INCREMENT,
+//             firstName VARCHAR(20),
+//             lastName VARCHAR(50),
+//             dob date,
+//	PRIMARY KEY (custID));
+//
+// INSERT INTO customers VALUES (101, 'Tom', 'Brady', '2018-11-11' );
+// INSERT INTO customers VALUES (107, 'Mary', 'Reilly', '2019-6-12');
+// INSERT INTO customers VALUES (105, 'James', 'Ryan', '2019-9-23');
+// INSERT INTO customers VALUES (106, 'Alice', 'O''Brien', '2019-12-1');
 
-INSERT INTO customers VALUES (101, 'Tom', 'Brady', '2018-11-11' );
-INSERT INTO customers VALUES (107, 'Mary', 'Reilly', '2019-6-12');
-INSERT INTO customers VALUES (105, 'James', 'Ryan', '2019-9-23');
-INSERT INTO customers VALUES (106, 'Alice', 'O''Brien', '2019-12-1');
-
- */
 
 import java.sql.*;
 
@@ -50,15 +48,21 @@ public class App
             String sqlQuery = "select * from customers";
             ResultSet resultSet = statement.executeQuery( sqlQuery );
 
+            // The ResultSet is a Java object that stores the results returned
+            // from the database (after executing the query)
+            // The ResultSet provides methods that allow us to access the
+            // results (rows of data) using Java code.
+
             // Iterate over the resultSet to process every row
             while ( resultSet.next() )
             {
-                // Columns can be identified by column name or by number
-                // The first column is number 1   e.g. resultSet.getString(2);
+                // Columns can be identified by column name OR by number
+                // The first column is number 1   e.g. resultSet.getString(1);
 
                 int customerId = resultSet.getInt("custID");
                 String firstName = resultSet.getString("firstName");
-
+                // resultSet.getXXX methods must match by type.
+                // ie. getInt() for INTEGER columns etc.
                 String lastName = resultSet.getString(3);  // get third value using index, i.e lastName
                 Date dob = resultSet.getDate("dob");
 
